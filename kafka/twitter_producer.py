df2c5fb89dda42f7bd0e51cd270e79c80c1693d2
Clairton Junior <clairton.menezes@gmail.com>
2021-03-21 19:22:20 -0300

    Added twitter producer

--- kafka/twitter_producer.py
+++ kafka/twitter_producer.py
@@ -1,11 +1,20 @@
 from kafka import KafkaProducer
 import json
+import time
+from kafka.errors import KafkaTimeoutError
+
 
 with open("../data/tweets.json") as jsonFile:
     data = json.load(jsonFile)
 
-print(data)
+producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+try:
+    for tweet in data:
+        producer.send('twitter', key=b'covid', value=tweet)
+        print('Sent:', tweet)
+        time.sleep(0.5)
+except KafkaTimeoutError:
+    print("Timeout: not possible to send the data.")
+finally:
+    producer.close()
 
-producer = KafkaProducer()
-
-future = producer.send('test-topic', b'Time do asses eh o melhor da Dell/Atlantico')
