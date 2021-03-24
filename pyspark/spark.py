293cba8821bcd8a2ec5d1febfc09f58633545d20
Clairton Menezes da S. Junior <clairton_menezes@virgo.atlantico.net.br>
2021-03-24 18:25:14 -0300

    fixed crash spark streaming due python version

--- pyspark/spark.py
+++ pyspark/spark.py
@@ -7,17 +7,14 @@ import os
 
 print(sys.version)
 
-os.environ['PYSPARK_SUBMIT_ARGS'] = ' --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.0 pyspark-shell'
+os.environ['PYSPARK_SUBMIT_ARGS'] = ' --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.4 pyspark-shell'
 
 sc = SparkContext(appName='Spark Streaming')
 ssc = StreamingContext(sc, 1)
 
 sc.setLogLevel("WARN")
 
-# kafka_stream = KafkaUtils.createStream(ssc,'localhost:2181','raw-event-streaming-consumer',{'test-topic':1})
-
 directKafkaStream = KafkaUtils.createDirectStream(ssc, ["test-topic"], {"metadata.broker.list": "localhost:9092", "zookeeper.connection.timeout.ms": "10000"})
-# kafka_stream.pprint()
 
 directKafkaStream.pprint()
 
