8318eb43648ada686afb7bdff0465eb559e8885e
Clairton Menezes da S. Junior <clairton_menezes@virgo.atlantico.net.br>
2021-03-24 13:26:48 -0300

    Added spark streaming

--- kafka/twitter_batch.py
+++ kafka/twitter_batch.py
@@ -15,6 +15,7 @@ auth = tweepy.OAuthHandler(API_KEY, API_TOKEN)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
+#procurar por covid, tweets que não foram replies ou retweets
 search = "covid -filter:replies -filter:retweets"
 
 results = tweepy.Cursor(api.search, q=search, since="2020-03-10",  result_type="mixed").items(1000)
--- pyspark/spark.py
+++ pyspark/spark.py
@@ -1,7 +1,25 @@
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
-from pyspark.streaming.util import kafka
+from pyspark.streaming.kafka import KafkaUtils
 import json
+import sys
+import os
 
-sc = SparkContext('localhost[*]', 'Spark Streaming')
+print(sys.version)
+
+os.environ['PYSPARK_SUBMIT_ARGS'] = ' --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.0 pyspark-shell'
+
+sc = SparkContext(appName='Spark Streaming')
 ssc = StreamingContext(sc, 1)
+
+sc.setLogLevel("WARN")
+
+# kafka_stream = KafkaUtils.createStream(ssc,'localhost:2181','raw-event-streaming-consumer',{'test-topic':1})
+
+directKafkaStream = KafkaUtils.createDirectStream(ssc, ["test-topic"], {"metadata.broker.list": "localhost:9092", "zookeeper.connection.timeout.ms": "10000"})
+# kafka_stream.pprint()
+
+directKafkaStream.pprint()
+
+ssc.start()
+ssc.awaitTermination()
